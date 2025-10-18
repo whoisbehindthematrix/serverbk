88166b733d69c325318df4b8243056161ec007ab
nkrider7 <narendranishad59@gmail.com>
2025-10-18 17:00:26 +0530

    init backend

---
+++ Project /backend-server/eslint.config.js
@@ -0,0 +1,15 @@
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["**/node_modules/**", "dist/**"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
